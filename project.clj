2bf6cb5aa9cf90b57c38e8f06159d415ccd8a8a2
Anton Kovalyov <keldzh@gmail.com>
2023-12-02 02:04:11 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject advent-of-code-2023 "0.0.0"
+  :description "My Advent of Code 2023 solutions in pursuit to learn Clojure."
+  :url "https://github.com/fAntel/AdventOfCode2023"
+  :license {:name "BSD-3-Clause"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot advent-of-code-2023.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
