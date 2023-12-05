7b945a9f3386ac770dd466743eb9fbf1730cac34
Anton Kovalyov <keldzh@gmail.com>
2023-12-05 04:51:20 +0300

    Advent Of Code 2023. Day 02: Cube Conundrum. Part 2

--- src/advent_of_code_2023/days/day_two.clj
+++ src/advent_of_code_2023/days/day_two.clj
@@ -11,11 +11,15 @@
   [game-lexem]
   (Integer/parseInt (apply str (drop game-prefix-length game-lexem))))
 
+(defn try-to-cubes-list
+  [try-as-str]
+  (let [cubes-as-strs (str/split (str/trim try-as-str) #",")
+        cubes-as-pairs (map #(str/split (str/trim %) #" ") cubes-as-strs)]
+    (map (fn [[count colour]] {:colour colour :count (Integer/parseInt count)}) cubes-as-pairs)))
+
 (defn try-to-cubes
   [try-as-str]
-  (let [cubes-as-strs (str/split try-as-str #",")
-        cubes-as-pairs (map #(str/split (str/trim %) #" ") cubes-as-strs)
-        cubes-list (map (fn [[count colour]] {:colour colour :count (Integer/parseInt count)}) cubes-as-pairs)]
+  (let [cubes-list (try-to-cubes-list try-as-str)]
     (reduce (fn [acc entry] (assoc acc (:colour entry) (+ (:count entry) (get acc (:colour entry) 0)))) {} cubes-list)))
 
 (defn possible-cubes?
@@ -24,27 +28,46 @@
 
 (defn possible-try?
   [try]
-  (let [cubes (try-to-cubes (str/trim try))]
+  (let [cubes (try-to-cubes try)]
     (every? #(possible-cubes? %) cubes)))
 
 (defn possible-game?
   [tries]
   (every? #(possible-try? %) tries))
 
-(defn game-to-number
+(defn parse-game-info
   [line]
   (let [[game-lexem tries-lexem] (str/split line #":")
         game-number (get-game-number game-lexem)
         tries (str/split tries-lexem #";")]
-    (if (possible-game? tries) game-number 0)))
+    {:game-number game-number :tries tries}))
+
+(defn game-to-number
+  [game-number tries]
+  (if (possible-game? tries) game-number 0))
+
+(defn least-possible-set-reduce-fun
+  [acc entry]
+  (let [colour (:colour entry)
+        count (:count entry)]
+    (assoc acc colour (max count (get acc colour 0)))))
+
+(defn power-of-game
+  [tries]
+  (let [cubes (flatten (map try-to-cubes-list tries))
+        least-possible-set (reduce least-possible-set-reduce-fun {} cubes)]
+    (reduce-kv (fn [acc k v] (if (zero? acc) v (* acc v))) 0 least-possible-set)))
 
 (defn part-one
   []
   (input/process-file-by-lines
    (input/compose-input-filename "day_two")
-   #(game-to-number %)
+   #(let [game-info (parse-game-info %)] (game-to-number (:game-number game-info) (:tries game-info)))
    +))
 
 (defn part-two
   []
-  nil)
+  (input/process-file-by-lines
+   (input/compose-input-filename "day_two")
+   #(let [game-info (parse-game-info %)] (power-of-game (:tries game-info)))
+   +))
