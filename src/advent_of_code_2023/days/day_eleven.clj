886584ab2143d712d6c8e5803a8aea1d0a9a145f
Anton Kovalyov <keldzh@gmail.com>
2024-01-13 23:18:26 +0300

    Advent Of Code 2023. Day 11: Cosmic Expansion. Part 1  * optimized

--- src/advent_of_code_2023/days/day_eleven.clj
+++ src/advent_of_code_2023/days/day_eleven.clj
@@ -1,99 +1,55 @@
 (ns advent-of-code-2023.days.day-eleven
   (:require [advent-of-code-2023.utils.input-parsers :as input]
-            [advent-of-code-2023.utils.debugging :as debugging]
             [clojure.string :as str]
-            [clojure.math.combinatorics :as combo]))
-
-(defn- get-column
-  [lines x height]
-  (map (fn [y] (nth (nth lines y) x)) (range height)))
-
-(defn- find-line-to-expand
-  [line-getter lines-count]
-  (->>
-   (range lines-count)
-   (map (fn [line-number] {:line-number line-number :line (line-getter line-number)}))
-   (filter (fn [{:keys [line]}] (apply = \. line)))
-   (map #(get % :line-number))
-   (sort)))
-
-(defn- lines-to-universe
-  [lines]
-  (flatten
-   (reduce-kv
-    (fn [acc y line]
-      (conj acc (map-indexed (fn [x c] {{:x x :y y} c}) line)))
-    {}
-    lines)))
-
-; TODO we don't release need full map. This function can be optimized if we find only galaxies with theirs coordinates
-; TODO and then increase x/y to expand.
-(defn- expand-universe
-  [lines]
-  (let [height            (count lines)
-        width             (count (first lines))
-        columns-to-expand (find-line-to-expand #(get-column lines % height) width)
-        rows-to-expand    (find-line-to-expand #(nth lines %) height)
-        empty-column      (repeat height \.)
-        expanded-columns  (reduce
-                           (fn [acc x]
-                             (if (some #{x} columns-to-expand)
-                               (conj acc empty-column empty-column)
-                               (conj acc (get-column lines x height))))
-                           []
-                           (range width))
-        empty-row          (repeat (count expanded-columns) \.)
-        expanded-universe  (reduce
-                            (fn [acc y]
-                              (if (some #{y} rows-to-expand)
-                                (conj acc empty-row empty-row)
-                                (conj acc (map #(nth % y) expanded-columns))))
-                            []
-                            (range height))]
-    expanded-universe))
+            [clojure.math.combinatorics :as combo]
+            [clojure.set]))
 
 (defn- universe-to-list-of-galaxies
-  [universe]
-  (let [height (count universe)
-        width  (count (first universe))]
-    (reduce
-     (fn [acc y]
-       (reduce
-        (fn [acc x]
-          (if (= \# (nth (nth universe y) x))
-            (conj acc {:x x :y y})
-            acc))
-        acc
-        (range width)))
-     #{}
-     (range height))))
+  ([universe]
+   (let [height (count universe)
+         width  (count (first universe))]
+     (universe-to-list-of-galaxies universe height width)))
+  ([universe height width]
+   (reduce
+    (fn [acc y]
+      (reduce
+       (fn [acc x]
+         (if (= \# (nth (nth universe y) x))
+           (conj acc {:x x :y y})
+           acc))
+       acc
+       (range width)))
+    #{}
+    (range height))))
 
-(defn- find-path-length
+(defn- lines-to-expand
+  [galaxies key line-size]
+  (clojure.set/difference (into (sorted-set) (range line-size)) (set (map key galaxies))))
+
+(defn- expand-universe
+  [universe]
+  (let [height            (count universe)
+        width             (count (first universe))
+        galaxies          (universe-to-list-of-galaxies universe height width)
+        columns-to-expand (lines-to-expand galaxies :x width)
+        rows-to-expand    (lines-to-expand galaxies :y height)]
+    (reduce
+     (fn [acc {:keys [x y]}]
+       (conj acc
+             {:x (+ x (count (subseq columns-to-expand < x)))
+              :y (+ y (count (subseq rows-to-expand < y)))}))
+     #{}
+     galaxies)))
+
+(defn- calculate-taxicab-geometry-distance
   [from to]
-  (cond
-    (= (:x from) (:x to)) (abs (- (:y to) (:y from)))
-    (= (:y from) (:y to)) (abs (- (:x to) (:x from)))
-    :else                 (loop [cur  {:x (min (:x from) (:x to)) :y (min (:y from) (:y to))}
-                                 dest {:x (max (:x from) (:x to)) :y (max (:y from) (:y to))}
-                                 length 0]
-                            (if (= cur dest)
-                              length
-                              (recur
-                                (let [cur-x  (:x cur)
-                                      cur-y  (:y cur)
-                                      x-diff (- (:x dest) cur-x)
-                                      y-diff (- (:y dest) cur-y)]
-                                  (if (> y-diff x-diff)
-                                    {:x cur-x :y (inc cur-y)}
-                                    {:x (inc cur-x) :y cur-y}))
-                                dest
-                                (inc length))))))
+  (+ (abs (- (:x from) (:x to))) (abs (- (:y from) (:y to)))))
 
 (defn- find-all-path-lengthes
   [galaxies]
   (->>
    (combo/combinations galaxies 2)
-   (map #(find-path-length (first %) (second %)))))
+   (map #(calculate-taxicab-geometry-distance (first %) (second %)))))
 
 (defn part-one
   []
@@ -101,8 +57,6 @@
    (input/compose-input-filename "day_eleven")
    (input/read-file-into-vector)
    (expand-universe)
-   ;(debugging/print-map)
-   (universe-to-list-of-galaxies)
    (find-all-path-lengthes)
    (apply +)))
 
